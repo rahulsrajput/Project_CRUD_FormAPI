d4d98076c4640088fe47a1c32307dc9b8ec7a586
rahulsrajput <rahulsrajput29@gmail.com>
2024-02-23 21:17:04 +0530

    Employee model and model register in admin.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    salary = models.IntegerField()
+    date_of_joining = models.DateField()
